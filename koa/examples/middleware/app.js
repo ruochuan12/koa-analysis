c95425680bf3de409156ed3327a36623509a576e
luoxiaochuan <luoxiaochuan@comteck.cn>
2020-02-23 23:25:27 +0800

    Add: 添加中间件例子 :tada:

---
+++ koa/examples/middleware/app.js
@@ -0,0 +1,31 @@
+const Koa = require('../../lib/application');
+
+// const Koa = require('koa');
+
+const app = new Koa();
+
+// x-response-time
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// logger
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+});
+
+// response
+
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(3000);
